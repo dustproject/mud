c57dc0e34f3ceb9658721e044cd4012c99ab9b07
Curtis Chong <curtischong5@gmail.com>
2023-06-21 15:26:47 -0400

    fix: IWorld.sol path resolution

--- packages/cli/src/utils/worldtypes.ts
+++ packages/cli/src/utils/worldtypes.ts
@@ -1,6 +1,7 @@
 import { getOutDirectory } from "@latticexyz/common/foundry";
 import path from "path";
 import { runTypeChain } from "typechain";
+import fs from "fs";
 
 /**
  * Generate IWorld typescript bindings
@@ -8,11 +9,15 @@ import { runTypeChain } from "typechain";
 export async function worldtypes() {
   const cwd = process.cwd();
   const forgeOurDir = await getOutDirectory();
+
   // Curtis changed this IWorldPath after we migrated to Yarn since we had a naming conflict (two IWorld.sol files): https://github.com/tenetxyz/voxel-aw/pull/45/files
   // const IWorldPath = path.join(process.cwd(), forgeOurDir, "IWorld.sol/IWorld.json");
-  const IWorldPath = path.join(process.cwd(), forgeOurDir, "world/IWorld.sol/IWorld.json");
-  console.log(cwd);
-  console.log(IWorldPath);
+  let IWorldPath = path.join(process.cwd(), forgeOurDir, "IWorld.sol/IWorld.json");
+
+  const IWorldDirPath = path.join(process.cwd(), forgeOurDir, "world"); // if the world directory exists, the IWorld.sol files are in here instead
+  if (fs.existsSync(IWorldDirPath)) {
+    IWorldPath = path.join(process.cwd(), forgeOurDir, "world/IWorld.sol/IWorld.json");
+  }
 
   await runTypeChain({
     cwd,

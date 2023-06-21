4bed93cbd08bfa7d63747c9fd61a6ebce4088475
Curtis Chong <curtischong5@gmail.com>
2023-06-21 15:37:19 -0400

    fix: undid change since you are only supposed to do yarn run deploy in extension-contracts

--- packages/cli/src/utils/worldtypes.ts
+++ packages/cli/src/utils/worldtypes.ts
@@ -1,7 +1,6 @@
 import { getOutDirectory } from "@latticexyz/common/foundry";
 import path from "path";
 import { runTypeChain } from "typechain";
-import fs from "fs";
 
 /**
  * Generate IWorld typescript bindings
@@ -9,15 +8,9 @@ import fs from "fs";
 export async function worldtypes() {
   const cwd = process.cwd();
   const forgeOurDir = await getOutDirectory();
-
   // Curtis changed this IWorldPath after we migrated to Yarn since we had a naming conflict (two IWorld.sol files): https://github.com/tenetxyz/voxel-aw/pull/45/files
   // const IWorldPath = path.join(process.cwd(), forgeOurDir, "IWorld.sol/IWorld.json");
-  let IWorldPath = path.join(process.cwd(), forgeOurDir, "IWorld.sol/IWorld.json");
-
-  const IWorldDirPath = path.join(process.cwd(), forgeOurDir, "world"); // if the world directory exists, the IWorld.sol files are in here instead
-  if (fs.existsSync(IWorldDirPath)) {
-    IWorldPath = path.join(process.cwd(), forgeOurDir, "world/IWorld.sol/IWorld.json");
-  }
+  const IWorldPath = path.join(process.cwd(), forgeOurDir, "world/IWorld.sol/IWorld.json");
 
   await runTypeChain({
     cwd,

846d8a6a495f7a63a9277c3de8b26cb8332a866d
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-08-03 16:07:35 +0100

    feat(store): registerAsRoot for tables

--- packages/cli/src/utils/deploy.ts
+++ packages/cli/src/utils/deploy.ts
@@ -182,11 +182,12 @@ export async function deploy(
   const tableIds: { [tableName: string]: Uint8Array } = {};
   promises = [
     ...promises,
-    ...Object.entries(mudConfig.tables).map(async ([tableName, { name, schema, keySchema }]) => {
-      console.log(chalk.blue(`Registering table ${tableName} at ${namespace}/${name}`));
+    ...Object.entries(mudConfig.tables).map(async ([tableName, { name, schema, keySchema, registerAsRoot }]) => {
+      const useNamespace = registerAsRoot === undefined || !registerAsRoot ? namespace : "";
+      console.log(chalk.blue(`Registering table ${tableName} at ${useNamespace}/${name}`));
 
       // Store the tableId for later use
-      tableIds[tableName] = toResourceSelector(namespace, name);
+      tableIds[tableName] = toResourceSelector(useNamespace, name);
 
       // Register table
       const schemaTypes = Object.values(schema).map((abiOrUserType) => {
@@ -202,7 +203,7 @@ export async function deploy(
       await fastTxExecute(
         WorldContract,
         "registerTable",
-        [toBytes16(namespace), toBytes16(name), encodeSchema(schemaTypes), encodeSchema(keyTypes)],
+        [toBytes16(useNamespace), toBytes16(name), encodeSchema(schemaTypes), encodeSchema(keyTypes)],
         confirmations
       );
 
@@ -210,7 +211,7 @@ export async function deploy(
       await fastTxExecute(
         WorldContract,
         "setMetadata(bytes16,bytes16,string,string[])",
-        [toBytes16(namespace), toBytes16(name), tableName, Object.keys(schema)],
+        [toBytes16(useNamespace), toBytes16(name), tableName, Object.keys(schema)],
         confirmations
       );
 

2012a22a0e5b9899996060b086f0f65846b2e019
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-03-01 11:15:28 +0000

    feat: compile to cjs

--- packages/cli/src/build.ts
+++ packages/cli/src/build.ts
@@ -5,7 +5,8 @@ import { World as WorldConfig } from "@latticexyz/world";
 import { worldToV1 } from "@latticexyz/world/config/v2";
 import { forge, getRemappings } from "@latticexyz/common/foundry";
 import { getExistingContracts } from "./utils/getExistingContracts";
-import { execa } from "execa";
+import { debug as parentDebug } from "./debug";
+import execa from "execa";
 
 type BuildOptions = {
   foundryProfile?: string;
